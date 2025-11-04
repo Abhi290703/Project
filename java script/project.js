e169c6e0c756148a07c62405feed33f54233d816
Abhi290703 <abhigec6@gmail.com>
2025-11-04 21:22:31 +0530

    add

--- java script/project.js
+++ java script/project.js
@@ -1,1 +1,11 @@
-const api = 
+fetch("https://www.themealdb.com/api/json/v1/1/categories.php")
+.then(response=>response.json()).then(data =>{
+    product = data.categories;
+    console.log(product);
+       products.map((product) => {
+                document.getElementById('container').innerHTML+=`<h4>${product.strCategory}</h4>`
+
+            })
+
+        }).catch(error => console.log('Error: data is not available')
+        )
